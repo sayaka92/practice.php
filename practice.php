4d39e62fb4dc8b8022061fd605d316f38d97aff8
EC2 Default User <ec2-user@ip-172-31-35-17.ap-northeast-1.compute.internal>
2023-01-19 17:33:22 +0000

    practice php

---
+++ practice.php
@@ -0,0 +1,36 @@
+<?php
+echo 'hello php!';
+<?php
+$a = 3;
+$b = 7;
+echo $a+$b;
+<?php
+$array_month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "Novenber", "December"]
+echo $array_month[7];
+<?php
+$hello = Hello;
+$name = inazawasayaka;
+$world = 's World!;
+echo $hello . "$name" . "world";
+<?php
+$tech_boost = tech;
+$tech_boost .=  boost;
+echo $tech_boost;
+<?php
+$calendar_2018 = [
+  "January" => "1月",
+  "February" => "2月",
+  "March" => "3月",
+  "April" => "4月",
+  "May" => "5月",
+  "June" => "6月",
+  "July" => "7月",
+  "August" => "8月",
+  "September" => "9月",
+  "October" => "10月",
+  "November" => "11月",
+  "December" => "12月"
+];
+
+// 12月を表示する
+echo $calendar_2018 [December];
